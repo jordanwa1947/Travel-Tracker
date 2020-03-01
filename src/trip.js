36727aebcb7de3b3fded731d5b7ec14650bcc6df
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-01 09:49:48 -0700

    Adds method that calculates the total cost spent on trips

--- src/trip.js
+++ src/trip.js
@@ -8,6 +8,19 @@ class Trip {
       return tripData.userID === userId;
     })
   }
+
+  calculateTotalSpentOnTrips(destinations, userId) {
+    const userTrips = this.findUserTrips(userId);
+    return userTrips.reduce((totalCost, userTrip) => {
+      let userDestination = destinations.find(destination => {
+        return destination.id === userTrip.destinationID
+      });
+      let lodgingCost = userDestination.estimatedLodgingCostPerDay * userTrip.duration * userTrip.travelers;
+      let travelCost = userDestination.estimatedFlightCostPerPerson * userTrip.travelers;
+      // console.log(lodgingCost, travelCost);
+      return totalCost + lodgingCost + travelCost;
+    }, 0)
+  }
 }
 
 module.exports = Trip;
