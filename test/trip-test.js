0a11429c12e03e91fadb9f3c1754b126d961b265
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-01 09:48:50 -0700

    Adds test for calculateTotalSpentOnTrips method

--- test/trip-test.js
+++ test/trip-test.js
@@ -46,4 +46,23 @@ describe('Trip Class', function() {
       }
     ])
   })
+
+  it('should be able to calculate the total a user has spent on trips', function() {
+    const destinations = [{
+      "id":49,
+      "destination":"Lima, Peru",
+      "estimatedLodgingCostPerDay":70,
+      "estimatedFlightCostPerPerson":400,
+      "image":"https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80","alt":"overview of city buildings with a clear sky"
+    },
+    {
+      "id":14,
+      "destination":"Lima, Peru",
+      "estimatedLodgingCostPerDay":70,
+      "estimatedFlightCostPerPerson":400,
+      "image":"https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80","alt":"overview of city buildings with a clear sky"
+    }]
+    const tripCost = new Trip(tripsData).calculateTotalSpentOnTrips(destinations, 43);
+    expect(tripCost).to.eq(3160);
+  });
 });
