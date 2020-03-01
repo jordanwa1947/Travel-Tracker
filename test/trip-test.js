545c56021599d82e2f5f962a1e0a1c8bb4da00ef
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-01 11:28:02 -0700

    Updates test data and adds test for calaculating the total spent on trips when no user id is given

--- test/trip-test.js
+++ test/trip-test.js
@@ -5,14 +5,20 @@ const Trip = require('../src/trip.js');
 
 describe('Trip Class', function() {
   let tripsData;
+  let destinations;
   beforeEach(function() {
     tripsData = [
       {"id":1,"userID":43,"destinationID":49,"travelers":1,"date":"2019/09/16","duration":8,"status":"approved","suggestedActivities":[]},
-      {"id":2,"userID":35,"destinationID":25,"travelers":5,"date":"2020/10/04","duration":18,"status":"pending","suggestedActivities":[]},
-      {"id":3,"userID":3,"destinationID":22,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
+      {"id":2,"userID":35,"destinationID":14,"travelers":5,"date":"2020/10/04","duration":18,"status":"pending","suggestedActivities":[]},
+      {"id":3,"userID":3,"destinationID":49,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
       {"id":4,"userID":43,"destinationID":14,"travelers":2,"date":"2020/02/25","duration":10,"status":"approved","suggestedActivities":[]},
-      {"id":5,"userID":42,"destinationID":29,"travelers":3,"date":"2020/04/30","duration":18,"status":"approved","suggestedActivities":[]},
-      {"id":6,"userID":29,"destinationID":35,"travelers":3,"date":"2020/06/29","duration":9,"status":"approved","suggestedActivities":[]}
+      {"id":5,"userID":42,"destinationID":14,"travelers":3,"date":"2020/04/30","duration":18,"status":"approved","suggestedActivities":[]},
+      {"id":6,"userID":29,"destinationID":14,"travelers":3,"date":"2020/06/29","duration":9,"status":"approved","suggestedActivities":[]}
+    ]
+
+    destinations = [
+      {"id":49, "destination":"Lima, Peru", "estimatedLodgingCostPerDay":70, "estimatedFlightCostPerPerson":400, "image":"https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80","alt":"overview of city buildings with a clear sky"},
+      {"id":14, "destination":"My House", "estimatedLodgingCostPerDay":30, "estimatedFlightCostPerPerson":100, "image":"https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80","alt":"overview of city buildings with a clear sky"}
     ]
   });
 
@@ -48,22 +54,13 @@ describe('Trip Class', function() {
   })
 
   it('should be able to calculate the total a user has spent on trips', function() {
-    const destinations = [{
-      "id":49,
-      "destination":"Lima, Peru",
-      "estimatedLodgingCostPerDay":70,
-      "estimatedFlightCostPerPerson":400,
-      "image":"https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80","alt":"overview of city buildings with a clear sky"
-    },
-    {
-      "id":14,
-      "destination":"Lima, Peru",
-      "estimatedLodgingCostPerDay":70,
-      "estimatedFlightCostPerPerson":400,
-      "image":"https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80","alt":"overview of city buildings with a clear sky"
-    }]
     const tripCost = new Trip(tripsData).calculateTotalSpentOnTrips(destinations, 43);
-    expect(tripCost).to.eq(3476);
+    expect(tripCost).to.eq(1936);
+  });
+
+  it('should calculate the total spent on all trips if no user id is given', function() {
+    const tripCost = new Trip(tripsData).calculateTotalSpentOnTrips(destinations);
+    expect(tripCost).to.eq(15785);
   });
 
   it('should be able to find pending trip requests for an agent', function() {
@@ -71,7 +68,7 @@ describe('Trip Class', function() {
     expect(pendingTrips).to.deep.eq([
       {
         "date": "2020/10/04",
-        "destinationID": 25,
+        "destinationID": 14,
         "duration": 18,
         "id": 2,
         "status": "pending",
@@ -81,7 +78,7 @@ describe('Trip Class', function() {
       },
       {
         "date": "2020/05/22",
-        "destinationID": 22,
+        "destinationID": 49,
         "duration": 17,
         "id": 3,
         "status": "pending",
